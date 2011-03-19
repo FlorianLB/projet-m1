26420677f53d76d6f2e6dcfa2b6312419b481be8
Bebert77270 <ju.aubert87@free.fr>
2011-03-19 23:50:39 +0100

    Modif parametre d'affichage UE

--- ogre/modules/ue/controllers/ue.classic.php
+++ ogre/modules/ue/controllers/ue.classic.php
@@ -6,6 +6,6 @@ class ueCtrl extends jControllerDaoCrud {
  
     protected $form = 'ue~ueform';
 	
-	protected $propertiesForList = array('code_ue', 'coeff', 'credits', 'libelle', 'last_version');
+	protected $propertiesForList = array('code_ue', 'coeff', 'credits', 'libelle');
  
 }
