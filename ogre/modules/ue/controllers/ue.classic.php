410edf1006f52589dc32b1057700ead309f3df0c
FlorianLB <f.lonqueu.brochard+github@gmail.com>
2011-03-20 11:56:14 +0100

    amélioration du last_version de UE

--- ogre/modules/ue/controllers/ue.classic.php
+++ ogre/modules/ue/controllers/ue.classic.php
@@ -5,7 +5,15 @@ class ueCtrl extends jControllerDaoCrud {
     protected $dao = 'ue~ue';
  
     protected $form = 'ue~ueform';
-	
-	protected $propertiesForList = array('code_ue', 'coeff', 'credits', 'libelle');
+    
+    protected $propertiesForList = array('code_ue', 'coeff', 'credits', 'libelle');
+ 
+ 
+    /**
+     * Après l'insertion de l'ue, on passe toute les ue avec le meme code en tant que non-active
+     */
+    protected function _afterCreate($form, $id, $resp){
+        jDao::get('ue~ue')->setOldUeVersion($id, $form->getData('code_ue'));
+    }
  
 }
