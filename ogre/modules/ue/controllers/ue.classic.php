05644b2aa4de7ffb96f515bf7cacc90603bd9d95
Bebert77270 <ju.aubert87@free.fr>
2011-03-20 11:52:45 +0100

    Correction creation nouvelle UE affection de last_version a 1 par default dans le controleur dao

--- ogre/modules/ue/controllers/ue.classic.php
+++ ogre/modules/ue/controllers/ue.classic.php
@@ -7,5 +7,9 @@ class ueCtrl extends jControllerDaoCrud {
     protected $form = 'ue~ueform';
 	
 	protected $propertiesForList = array('code_ue', 'coeff', 'credits', 'libelle');
+	
+	protected function _beforeSaveCreate($form, $form_daorec){
+        $form_daorec->last_version = 1;
+    }
  
 }
