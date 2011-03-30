1be10e67b00f862798184559628f827e06c4661c
Bebert77270 <ju.aubert87@gmail.com>
2011-03-30 14:11:51 +0200

    Affichage formation dans modification etudiant

--- ogre/modules/etudiants/controllers/etudiants.classic.php
+++ ogre/modules/etudiants/controllers/etudiants.classic.php
@@ -52,18 +52,18 @@ class etudiantsCtrl extends jControllerDaoCrud {
 	    $formation = $factoryformation->get($semestre->id_formation);
 	    $formationarray[$formation->id_formation] = $formation;
 	}	    
-	$form->deactivate('formation');
+	$form->deactivate('formations');
 	$tpl->assign('formations', $formationarray);
     }
    
     public function _create($form, $resp, $tpl){
         $resp->setTitle('Créer un nouvel etudiant');
-	$form->deactivate('formation',FALSE);
+	$form->deactivate('formations',FALSE);
     }
    
     public function _editUpdate($form, $resp, $tpl){
        $resp->setTitle('Modifier un etudiant');
-       $form->deactivate('formation',FALSE);
+       $form->deactivate('formations',FALSE);
     }  
     /**
      * Suppresion des elements dependants de etudiants
