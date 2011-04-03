87c47364c4da694ea5d8013b5ed584fdfd1b42c9
tobyte <lazzarino.remi@gmail.com>
2011-04-04 01:23:51 +0200

    supression de fonction import pv pour presentation client

--- ogre/modules/ogre/controllers/default.classic.php
+++ ogre/modules/ogre/controllers/default.classic.php
@@ -25,15 +25,15 @@ class defaultCtrl extends jController {
             <ul>
                     <li><a href="'.jUrl::get('formations~formations:index').'">Formations CRUD</a></li>
 	<li><a href="'.jUrl::get('ue~ue:index').'">Ue CRUD</a></li>
-                    <li><a href="'.jUrl::get('etudiants~import:index').'">Import APOGEE</a></li>
-	<li><a href="'.jUrl::get('etudiants~import:index2').'">Import PV</a></li>
+
+	<!--<li><a href="'.jUrl::get('etudiants~import:index2').'">Import PV</a></li> -->
                     <li><a href="'.jUrl::get('ue~saisie_epreuve:intro').'">Saisie notes/epreuve</a></li>
             </ul>
         ';
 	
         $content.='<div class="widget-container">';
         $content.= jZone::get('etudiants~recherche');
-         $content.= jZone::get('etudiants~widget_etudiant');
+        $content.= jZone::get('etudiants~widget_etudiant');
         $content.='</div>';
         
 	$rep->body->assign('MAIN', $content);
