879539cd9b2372dbe6d3194fbfd963aac54a1a61
FlorianLB <f.lonqueu.brochard+github@gmail.com>
2011-04-03 11:55:32 +0200

    tests unitaires sur Formule Ue

--- ogre/modules/utils/classes/Formule.class.php
+++ ogre/modules/utils/classes/Formule.class.php
@@ -3,6 +3,10 @@
 class Formule{
     
     public static function parseFormuleUe($formule){
+        
+        if(!is_string($formule))
+            return;
+        
         $result = array();
         preg_match_all('/([0-9])?[ *]*([a-zA-Z]+[0-9]?)/',$formule, $result);
         
---
+++ ogre/modules/utils/tests/ogre.utils_formule.html_cli.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class testUtilsFormule extends UnitTestCase {
+ 
+    function testParseFormuleUe() {
+        jClasses::inc('utils~Formule');
+        
+        //Test1
+        $this->assertIdentical(Formule::parseFormuleUe(78), null);
+       
+       
+        //Test2
+        $formule = '2PA1 + 3 PA2 + EvC';
+        $resultat_attendu = array(
+            array('2PA1', '3 PA2', ' EvC'),
+            array('2', '3', ''),
+            array('PA1', 'PA2', 'EvC')
+        );
+        $this->assertIdentical(Formule::parseFormuleUe($formule), $resultat_attendu);
+    
+        //Test3
+        $formule = ' PA + 2*EvC + 1* TP';
+        $resultat_attendu = array(
+            array(' PA', '2*EvC', '1* TP'),
+            array('', '2', '1'),
+            array('PA', 'EvC', 'TP')
+        );
+        $this->assertIdentical(Formule::parseFormuleUe($formule), $resultat_attendu);
+    
+    }
+}
