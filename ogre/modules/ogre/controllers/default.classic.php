480e95a8ba3a10cb894091cb9669aebb8a84e653
FlorianLB <f.lonqueu.brochard+github@gmail.com>
2011-04-04 21:12:44 +0200

    infos page d'accueil pour les testeurs

--- ogre/modules/ogre/controllers/default.classic.php
+++ ogre/modules/ogre/controllers/default.classic.php
@@ -37,7 +37,28 @@ class defaultCtrl extends jController {
         $content.= jZone::get('ue~widget_note');
         $content.='</div>';
         
-	$rep->body->assign('MAIN', $content);
+        $content .= '<h3 style="margin-top:50px;">A destination des testeurs</h3>
+        
+        <p> L\'application dans son état actuelle <strong>ne gère pas encore</strong> : </p>
+       
+        <ul class="liste">
+            <li>Les cas particuliers au niveau des étudiants (formule spéciale pour les UEs, etc)</li>
+            <li>L\'import des anciennes notes</li>
+            <li>L\'historique d\'un etudiant (ne marche que partiellement)</li>
+            <li>L\'affichage et l\'export des procès verbaux pour les jurys</li>
+        </ul>
+        
+        <p>Bugs connus: </p>
+         <ul class="liste">
+            <li>Toutes modifications d\'un étudiant entraine la suppresion de ces options</li>
+        </ul>
+        
+        ';
+        
+        
+        
+        
+        $rep->body->assign('MAIN', $content);
 
 
         return $rep;
