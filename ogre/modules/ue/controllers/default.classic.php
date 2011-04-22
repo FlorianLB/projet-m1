0f0bbd2daee5418aa8738c048fa7bd3e5edb2937
tobyte <lazzarino.remi@gmail.com>
2011-04-22 14:04:58 +0200

    merge

--- ogre/modules/ue/controllers/default.classic.php
+++ ogre/modules/ue/controllers/default.classic.php
@@ -14,9 +14,10 @@ class defaultCtrl extends jController {
     */
     function index() {
         $rep = $this->getResponse('html');
-        jClasses::inc('utils~customSQL');
+       // jClasses::inc('utils~customSQL');
+        //jClasses::inc('utils~Formule');
         
-        ////$var = Formule::parseFormuleUe('SUP ( 2PA1 + 2 PE2 + 3 * evc)');
+       //$var = Formule::parseFormuleUe('SUP ( 2PA1 + 2 PE2 + 3 * evc)');
         //$vars = jDao::get('ue~epreuve')->getByUeAndType(9,"CTP1");
         //jLog::dump($vars);
         //foreach($vars as $var){
@@ -24,10 +25,20 @@ class defaultCtrl extends jController {
         //    jLog::dump($var);
         //}
         
-        var_dump(customSQL::noteExisteDeja(33
-                                           ,1,10800755));
+        //var_dump(customSQL::noteExisteDeja(33
+        //                                   ,1,10800755));
         
         //
+        
+        
+       // $var = Formule::sup('Sup ((EvC + P2 ) /2 ; P2)');
+       
+       //$toto = jDao::get('formations~formation')->getLastFormation();
+        //foreach($toto as $ligne){
+        //$annee = substr("2000-2001",5,4);
+        //$annee .= "-".($annee + 1);
+        //    var_dump($annee);
+        //}
        // $rep->body->assign('MAIN',$content);
         return $rep;
     }
