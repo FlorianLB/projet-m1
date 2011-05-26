d54e5781f18858c1803fe85f6ac3728b83c98c36
Hector47 <reviosoft@gmail.com>
2011-05-26 21:12:11 +0200

    mise a jour du statut de l'étudiant lors de la reimportation du PV

--- ogre/modules/phpexcel/controllers/import.classic.php
+++ ogre/modules/phpexcel/controllers/import.classic.php
@@ -25,6 +25,7 @@ class importCtrl extends jController {
         $rep = $this->getResponse('redirect');
         $rep->action = 'ogre~index';
         $nbnotemodifierr = 0;
+        $nbEleveAdmis = 0;
         $name = $_FILES['excel_pv']['tmp_name'];
         try {
             /** Load $inputFileName to a PHPExcel Object  **/
@@ -37,19 +38,49 @@ class importCtrl extends jController {
                 //jLog::dump($row3->getName(),'une notes');
                 $nomCeluleNomee = $row3->getName();
                 list($h, $id_epreuve, $num_etudiant, $id_semestre) = explode("_", $nomCeluleNomee);
-                if($id_epreuve != "PTJURRY"){
-                    $record = $factory_notes->get($id_epreuve, $num_etudiant, $id_semestre);
-                    //$case = ;
-                    $note = $row3->getWorksheet()->getCell($row3->getRange())->getValue();
-                    if($record->valeur != $note){
-                        
-                        $record->valeur = $note;
-                        $factory_notes->update($record);
-                        $nbnotemodifierr++;
-                        
-                    }
+                switch($id_epreuve){
+                    case 'PTJURRY' :
+                        //TODO
+                        break;
+                    case 'MoyGen' :
+                        //faire ici la vaalidation du semestre
+                        $moyenne = $row3->getWorksheet()->getCell($row3->getRange())->getCalculatedValue();
+                        if($moyenne >= 10 ){
+                            $nbEleveAdmis++;
+                            $factory_etudiant_semestre = jDao::get('etudiants~etudiants_semestre');
+                            
+                            $conditions = jDao::createConditions();
+                            $conditions->addCondition('num_etudiant', '=', $num_etudiant);
+                            $conditions->addCondition('id_semestre', '=', $id_semestre);
+                            
+                            $unsemestreetudiant = $factory_etudiant_semestre->findBy($conditions);
+
+                            foreach ($unsemestreetudiant as $row) {
+                                $row->statut = 'ADM';
+                                $factory_etudiant_semestre->update($row);
+                            }
+                        }
+                        jLog::dump($moyenne);
+                        break;
                     
-                }            
+                    default :
+                        $record = $factory_notes->get($id_epreuve, $num_etudiant, $id_semestre);
+                        //$case = ;
+                        $note = $row3->getWorksheet()->getCell($row3->getRange())->getValue();
+                        if($note == 'ABS'){$note = -1;}                      
+                        if($record->valeur != $note){
+                            $record->valeur = $note;
+                            $factory_notes->update($record);
+                            $nbnotemodifierr++;
+                            
+                        }
+                       
+                        break;
+                }   
+                //if($id_epreuve != "PTJURRY"){
+                //
+                //    
+                //}            
                 
             }
             
@@ -57,17 +88,26 @@ class importCtrl extends jController {
             jMessage::add('Erreur lors du chargement du fichier : '.$e->getMessage(),'error' );
             return $rep;
         }
+        $message = '';
         if ($nbnotemodifierr == 1){
             
-            jMessage::add("L'importation du PV a reussie (1 note a été modifiée)");
+            $message = "L'importation du PV a reussie (une note a été modifiée). ";
             
         }else{
             
-            jMessage::add("L'importation du PV a reussie ($nbnotemodifierr notes ont été modifiées )");
+            $message = "L'importation du PV a reussie ($nbnotemodifierr notes ont été modifiées ). ";
             
         }
-        
-        
+        if ($nbEleveAdmis == 1){
+            
+            $message = $message . "Un étudiant a été admis";
+            
+        }else{
+            
+             $message = $message . "$nbEleveAdmis étudiants ont été admis";
+            
+        }        
+        jMessage::add($message);
         return $rep;
     }
  
